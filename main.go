b3a5f44058836fcd277da862eb416f5208ec43b6
Bartlomiej Plotka <bwplotka@gmail.com>
2020-09-01 19:13:09 +0100

    Initial commit.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/oklog/run"
+	"github.com/pkg/errors"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/thanos-community/dzieki-mellon/pkg/extkingpin"
+)
+
+const (
+	logFormatLogfmt = "logfmt"
+	logFormatJson   = "json"
+)
+
+func setupLogger(logLevel, logFormat string) log.Logger {
+	var lvl level.Option
+	switch logLevel {
+	case "error":
+		lvl = level.AllowError()
+	case "warn":
+		lvl = level.AllowWarn()
+	case "info":
+		lvl = level.AllowInfo()
+	case "debug":
+		lvl = level.AllowDebug()
+	default:
+		panic("unexpected log level")
+	}
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	if logFormat == logFormatJson {
+		logger = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+	}
+	logger = level.NewFilter(logger, lvl)
+	return log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+}
+
+func main() {
+	app := extkingpin.NewApp(kingpin.New(filepath.Base(os.Args[0]), `TBD.
+
+Features:
+
+`).Version("yolo"))
+	logLevel := app.Flag("log.level", "Log filtering level.").
+		Default("info").Enum("error", "warn", "info", "debug")
+	logFormat := app.Flag("log.format", "Log format to use. Possible options: logfmt or json.").
+		Default(logFormatLogfmt).Enum(logFormatLogfmt, logFormatJson)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	registerTBD(ctx, app)
+
+	cmd, runner := app.Parse()
+	logger := setupLogger(*logLevel, *logFormat)
+
+	var g run.Group
+	g.Add(func() error {
+		return runner(nil, logger, nil, nil, nil, false)
+	}, func(err error) {
+		cancel()
+	})
+
+	// Listen for termination signals.
+	{
+		cancel := make(chan struct{})
+		g.Add(func() error {
+			return interrupt(logger, cancel)
+		}, func(error) {
+			close(cancel)
+		})
+	}
+
+	if err := g.Run(); err != nil {
+		// Use %+v for github.com/pkg/errors error to print with stack.
+		level.Error(logger).Log("err", fmt.Sprintf("%+v", errors.Wrapf(err, "%s command failed", cmd)))
+		os.Exit(1)
+	}
+	level.Info(logger).Log("msg", "exiting")
+}
+
+func interrupt(logger log.Logger, cancel <-chan struct{}) error {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case s := <-c:
+		level.Info(logger).Log("msg", "caught signal. Exiting.", "signal", s)
+		return nil
+	case <-cancel:
+		return errors.New("canceled")
+	}
+}
+
+func registerTBD(ctx context.Context, app *extkingpin.App) {
+	cmd := app.Command("tbd", "yolo.")
+	cmd.Setup(func(ctx) error {
+		fmt.Println("yolo")
+		return nil
+	})
+}
+
---
+++ pkg/version/version.go
@@ -0,0 +1,4 @@
+package version
+
+// Version returns 'dzieki-million' version.
+const Version = "v0.0.0-dev"
